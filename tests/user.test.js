ab566889c36e14fad1a316247e0e85a893a869ed
Igor Salgado <igorssalgado@icloud.com>
2019-08-25 09:04:16 -0300

    little advanced test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,36 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Taynara',
+    email: 'taynara@ex.com',
+    password: '1234568!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
         name: 'IRGU',
-        email: 'irguu_@hotmail.com',
+        email: 'igorssalgado@icloud.com',
         password: '1234568!'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexisting user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email  + 'wrong',
+        password: userOne.password + 'wrong'
+    }).expect(400)
 })
