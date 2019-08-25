2a9c28450413cad0926c743f3d8f60cb986a830f
Igor Salgado <igorssalgado@icloud.com>
2019-08-25 11:38:31 -0300

    Advanced Assertions with DB

--- tests/user.test.js
+++ tests/user.test.js
@@ -43,11 +43,15 @@ test('Should signup a new user', async () => {
 })
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
             email: userOne.email,
             password: userOne.password,
     })
     .expect(200)
+
+    const user = await User.findById(userOneId);
+
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login nonexisting user', async () => {
@@ -73,12 +77,16 @@ test('Should not get profile for unauthenticated user', async () => {
         .expect(401)
 })
 
-test('Should delete account for user', async () => {
+test('Should delete account for user', async () => {   
     await request(app)
         .delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+
+        const user = await User.findById(userOneId);
+        expect(user).toBeNull();
+      
 })
 
 test('Should NOT delete account for user', async () => {
